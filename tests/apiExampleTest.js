71dea571a11e83c2a0e8b3535c62509a535f08f5
Dmitry <saiferdima@gmail.com>
2018-07-14 21:07:19 +0300

    Api sample

---
+++ tests/apiExampleTest.js
@@ -0,0 +1,42 @@
+module.exports = {
+    //TBD
+    '@disabled': true,
+    'Jira login': (client) => {
+        let login = client.globals.adminLogin;
+        let password = client.globals.adminPsw;
+        let loginPage = client.page.loginPage();
+        loginPage.navigate();
+        loginPage.login(login, password);
+    },
+
+    "Retrieve API (GET)": function (client) {
+        var apiUrl = 'http://localhost:8080/rest/api/2/issue/createmeta';
+        client.apiGet(apiUrl, function (response) {
+            console.log(response.body);
+            var data = JSON.parse(response.body);
+            console.log(data.status);
+
+            client.assert.equal(response.statusCode, 200, "200 OK");
+
+            client.end();
+        });
+
+    },
+
+    // "Retrieve API (POST)": function (client) {
+    //     var apiUrl = 'http://localhost:8080/rest/api/2/issue/';
+    //     var postData = {'lorem':'ipsum'};
+    //     var postHeaders = {'Content-Type': 'application/json'};
+    //     client.apiPost(apiUrl, postData,postHeaders,'', function (response) {
+    //         console.log(response.body);
+    //         var data = JSON.parse(response.body);
+    //         console.log(data.status);
+    //
+    //         client.assert.equal(response.statusCode, 200, "200 OK");
+    //
+    //         client.end();
+    //     });
+    //
+    // }
+
+};
